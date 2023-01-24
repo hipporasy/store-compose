132bdacbf349fff94c03c84c2c2a25b01e35eefb
Rasy <rasy@Rasys-MacBook-Pro.local>
2023-01-24 15:37:46 +1100

    first commit

---
+++ app/src/main/java/codes/hipporasy/store/StoreApp.kt
@@ -0,0 +1,7 @@
+package codes.hipporasy.store
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class StoreApp : Application()
---
+++ app/src/main/java/codes/hipporasy/store/presentation/HomeScreen.kt
@@ -0,0 +1,25 @@
+package codes.hipporasy.store.presentation
+
+import androidx.compose.runtime.Composable
+import com.ramcosta.composedestinations.annotation.Destination
+import com.ramcosta.composedestinations.annotation.RootNavGraph
+import com.ramcosta.composedestinations.navigation.DestinationsNavigator
+
+@RootNavGraph(start = true) // sets this as the start destination of the default nav graph
+@Destination
+@Composable
+fun HomeScreen(
+    navigator: DestinationsNavigator
+) {
+    /*...*/
+//    navigator.navigate(ProfileScreenDestination(id = 7, groupName = "Kotlin programmers"))
+
+}
+
+@Destination
+@Composable
+fun ProfileScreen(
+    id: Int, // <-- required navigation argument
+    groupName: String?, // <-- optional navigation argument
+    isOwnUser: Boolean = false // <-- optional navigation argument
+) { /*...*/ }
